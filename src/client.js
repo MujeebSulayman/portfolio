98a6f1461b9743b3f02ab9ed5f927eb75330b550
Hemjaytech <sulaymanmujeeb6@gmail.com>
2023-03-15 18:49:45 -0700

    Enter

--- src/client.js
+++ src/client.js
@@ -4,4 +4,5 @@ export default sanityClient({
   projectId: 'u71ecgpw', // find this at manage.sanity.io or in your sanity.json
   dataset: 'production', // this is from those question during 'sanity init'
   useCdn: true,
+  token: process.env.PUBLIC_SANITY
 });
