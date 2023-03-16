779a56dac5c85488450488da40adf087249ccf18
Hemjaytech <sulaymanmujeeb6@gmail.com>
2023-03-16 03:09:59 -0700

    Done

--- sanityportfolio/sanity.cli.js
+++ sanityportfolio/sanity.cli.js
@@ -4,7 +4,5 @@ export default defineCliConfig({
   api: {
     projectId: 'u71ecgpw',
     dataset: 'production',
-    token:
-      'sk8bOJrLxXgMLlauthRRKHs2RWzeu3qK0zob2pNklQdzFHTLsak3obGB9e80zaAtiwmDQPctoxHxTHR6xwSnVz7FM4ocrLa3Yg58HiifOXq30bL5yHa5puldmTaoX1uhqOH7KB42vasTQiIocxP9CGjqItWCwLTMoYRW7YG5e3e73H3aAcSB',
   },
 })
