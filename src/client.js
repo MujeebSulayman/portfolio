642dfbc930ef157426357364e7ba58065e3b0909
Hemjaytech <sulaymanmujeeb6@gmail.com>
2023-03-15 18:51:03 -0700

    Enter

--- src/client.js
+++ src/client.js
@@ -4,5 +4,6 @@ export default sanityClient({
   projectId: 'u71ecgpw', // find this at manage.sanity.io or in your sanity.json
   dataset: 'production', // this is from those question during 'sanity init'
   useCdn: true,
-  token: process.env.PUBLIC_SANITY
+  token:
+    'skZcR2Bre5EAAfnFs5iXojaSCe1FoQrGOMeZ9TDioOg0f5UVWReaI3Ii9KBBqDkKIq0LhDhh3onKo0M4yRi8cTG8INvnG5pP0GuqPXOKdrHyqK9rJr6714QiKQiLjQORmQbWTfucLDCtI93f8l3ERGV6oWdgzhTtdDNA34J7XyYOxEDccvS4',
 });
